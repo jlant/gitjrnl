30c0f87b0f647a7d636ecba8e9fa7f4d3ddc47f8
Jeremiah Lant <jeremiahlant@gmail.com>
2019-09-18 09:19:24 -0400

    Update gitjrnl.sh.
    The journal.txt file will be one git log behind the current git log.

--- gitjrnl.sh
+++ gitjrnl.sh
@@ -3,7 +3,7 @@
 filename="journal.txt"
 entry="$1"
 
-git add .
-git commit -m "$entry"
 git log -1 --pretty=format:'%Cgreen%ad %Creset: %s' --date=short >> $filename
 echo "" >> $filename
+git add .
+git commit -m "$entry"
